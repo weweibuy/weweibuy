11b0aac7466cd3219edec70cc3c0d05fef066981
ak514250 <514250798@qq.com>
2018-09-28 23:30:27 +0800

    创建工程

--- generatorSqlmapCustom/src/GeneratorSqlmap.java
+++ generatorSqlmapCustom/src/GeneratorSqlmap.java
@@ -1,15 +1,13 @@
 
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
-import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GeneratorSqlmap {
 
