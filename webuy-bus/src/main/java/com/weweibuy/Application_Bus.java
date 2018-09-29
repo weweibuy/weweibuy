90188b0a39105da53de90f32ffccf1f319efd774
ak514250 <ak514250@126.com>
2018-09-29 23:14:17 +0800

    增加bus 工程

---
+++ webuy-bus/src/main/java/com/weweibuy/Application_Bus.java
@@ -0,0 +1,10 @@
+package com.weweibuy;
+
+/**
+ * @ClassName Application_Bus
+ * @Description: TODO
+ * @Author durenhao  ak514250@126.com
+ * @Date 2018/9/29 23:13
+ */
+public class Application_Bus {
+}
