bc2f258d0f12efe452ed994b13f20a7844821ef4
杜仁浩 <ak514250@126.com>
2018-10-27 00:31:42 +0800

    增加admin工程

---
+++ webuy-admin/src/main/java/com/weweibuy/Application_Admin.java
@@ -0,0 +1,24 @@
+package com.weweibuy;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName Application_Admin
+ * @Description
+ * @Author durenhao
+ * @Date 2018/10/26 23:43
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
+@EnableDiscoveryClient
+public class Application_Admin {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application_Admin.class);
+    }
+}
