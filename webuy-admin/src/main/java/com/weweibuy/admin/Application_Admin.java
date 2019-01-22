86dff2e8140d38cdf315c80210596b8e32ff4b9a
杜仁浩 <ak514250@126.com>
2019-01-22 23:02:20 +0800

    跟改admin工程相关配置

--- webuy-admin/src/main/java/com/weweibuy/admin/Application_Admin.java
+++ webuy-admin/src/main/java/com/weweibuy/admin/Application_Admin.java
@@ -2,11 +2,10 @@ package com.weweibuy.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * @ClassName Application_Admin
@@ -14,11 +13,10 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
  * @Author durenhao
  * @Date 2018/10/26 23:43
  **/
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
-@EnableHystrixDashboard
-@EnableTurbine
 @EnableAdminServer
 public class Application_Admin {
 
--- webuy-admin/src/main/java/com/weweibuy/admin/config/WebSecurityConfig.java
+++ webuy-admin/src/main/java/com/weweibuy/admin/config/WebSecurityConfig.java
@@ -1,66 +1,37 @@
-//package com.weweibuy.admin.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//
-///**
-// * @ClassName WebSecurityConfig
-// * @Description
-// * @Author durenhao
-// * @Date 2019/1/21 23:28
-// **/
-//@Configuration
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Autowired
-//    private UserDetailsService userDetailsService;
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userDetailsService);
-//    }
-//
-//    @Override
-//    public void configure(WebSecurity web) throws Exception {
-//        //忽略css.jq.img等文件
-//        web.ignoring().antMatchers("/**.html","/**.css", "/img/**", "/**.js","/third-party/**");
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//        http
-//                .csrf().disable() //HTTP with Disable CSRF
-//                .authorizeRequests() //Authorize Request Configuration
-//                .antMatchers( "/login",
-//                        "/api/**",
-//                        "/**/heapdump",
-//                        "/**/loggers",
-//                        "/**/liquibase",
-//                        "/**/logfile",
-//                        "/**/flyway",
-//                        "/**/auditevents",
-//                        "/**/jolokia").permitAll() //放开"/api/**"：为了给被监控端免登录注册并解决Log与Logger冲突
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/**").hasRole("USER")
-//                .antMatchers("/**").authenticated()
-//                .and() //Login Form configuration for all others
-//                .formLogin()
-//                .loginPage("/login.html")
-//                .loginProcessingUrl("/login").permitAll()
-//                .defaultSuccessUrl("/")
-//                .and() //Logout Form configuration
-//                .logout()
-//                .deleteCookies("remove")
-//                .logoutSuccessUrl("/login.html").permitAll()
-//                .and()
-//                .httpBasic().disable();
-//    }
-//
-//}
+package com.weweibuy.admin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+/**
+ * @ClassName WebSecurityConfig
+ * @Description
+ * @Author durenhao
+ * @Date 2019/1/21 23:28
+ **/
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl("/");
+
+        http.authorizeRequests()
+                .antMatchers( "/assets/**").permitAll()
+                .antMatchers( "/login",
+                        "/instances/**",
+                        "/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage( "/login").successHandler(successHandler).and()
+                .logout().logoutUrl( "/logout").and()
+                .httpBasic().disable()
+                .csrf().disable();
+    }
+
+}
