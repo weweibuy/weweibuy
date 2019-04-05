b737032313c4d434a991d338ad9794fddd29ad82
ak514250 <ak514250@126.com>
2019-04-05 20:26:10 +0800

    更改工程结构

---
+++ webuy-backstage/src/main/java/com/weweibuy/webuy/backstage/Application_Backstage.java
@@ -0,0 +1,33 @@
+package com.weweibuy.webuy.backstage;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
+
+/**
+ * @ClassName Application
+ * @Description TODO
+ * @Author durenhao
+ * @Date 2018/9/21 23:07
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.weweibuy.webuy.product.client")
+@EnableHystrix
+@EnableRetry
+@MapperScan(basePackages = {"com.weweibuy.webuy.backstage.mapper"})
+@EnableCaching
+public class Application_Backstage {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application_Backstage.class, args);
+    }
+
+}
