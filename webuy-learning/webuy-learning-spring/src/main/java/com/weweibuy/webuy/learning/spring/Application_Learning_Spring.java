16e8088e050ff0eaceee0db6d48c56fe4a694298
ak514250 <ak514250@126.com>
2019-04-07 09:51:25 +0800

    使用 apollo 配置

--- webuy-learning/webuy-learning-spring/src/main/java/com/weweibuy/webuy/learning/spring/Application_Learning_Spring.java
+++ webuy-learning/webuy-learning-spring/src/main/java/com/weweibuy/webuy/learning/spring/Application_Learning_Spring.java
@@ -1,13 +1,19 @@
 package com.weweibuy.webuy.learning.spring;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author durenhao
  * @date 2019/4/7 0:02
  **/
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableApolloConfig
 public class Application_Learning_Spring {
 
     public static void main(String[] args) {
