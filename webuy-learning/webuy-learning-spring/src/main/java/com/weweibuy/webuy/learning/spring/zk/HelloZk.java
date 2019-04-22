84804e10530eb766d4ef81947c6c17fa0ef0a80e
ak514250 <ak514250@126.com>
2019-04-23 00:13:12 +0800

    测试zk

--- webuy-learning/webuy-learning-spring/src/main/java/com/weweibuy/webuy/learning/spring/zk/HelloZk.java
+++ webuy-learning/webuy-learning-spring/src/main/java/com/weweibuy/webuy/learning/spring/zk/HelloZk.java
@@ -1,12 +1,13 @@
 package com.weweibuy.webuy.learning.spring.zk;
 
-import com.weweibuy.webuy.learning.spring.zk.session.ZkSessionHolder;
 import lombok.extern.slf4j.Slf4j;
-import org.I0Itec.zkclient.IZkDataListener;
-import org.I0Itec.zkclient.ZkClient;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.data.Stat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,74 +22,167 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/zk")
 public class HelloZk {
 
-    @RequestMapping("/lock-p")
-    public String helloZk(String node) {
-        ZkClient zkClient = ZkSessionHolder.getZooKeeper();
-        zkClient.createPersistent("/test/lock-" + node, true);
-        return "zk..";
-    }
+    /*
+     *   Zookeeper的节点创建模式：
+     *   PERSISTENT：持久化
+     *   PERSISTENT_SEQUENTIAL：持久化并且带序列号
+     *   EPHEMERAL：临时
+     *   EPHEMERAL_SEQUENTIAL：临时并且带序列号
+     */
 
-    @RequestMapping("/lock")
-    public String helloZk2(String node) {
-        ZkClient zkClient = ZkSessionHolder.getZooKeeper();
+
+    @Autowired
+    private CuratorFramework curatorFramework;
+
+    /**
+     * 创建持久化节点
+     *
+     * @param node
+     * @return
+     */
+    @RequestMapping("/create-node")
+    public String create(String node) {
         try {
-            zkClient.create("/test/lock-" + node, System.currentTimeMillis(), CreateMode.EPHEMERAL);
+            // 默认为持久化创建
+            return curatorFramework.create()
+                    .forPath("/" + node, "你好zk".getBytes());
+        }catch (KeeperException.NodeExistsException e){
+            log.error("节点已经存在");
+            return "节点已经存在";
         } catch (Exception e) {
-            if (e instanceof KeeperException.NodeExistsException) {
-                return "fail get lock";
-            }
+            log.error("zk 创建节点异常: {}", e.getMessage());
+            return "fail: " + e.getMessage();
         }
-        return "zk..";
     }
 
-    @RequestMapping("/get-data")
-    public String helloZk3(String node) {
-        ZkClient zkClient = ZkSessionHolder.getZooKeeper();
-        Object o = zkClient.readData("/test/lock-" + node);
-        Stat stat = new Stat();
-        zkClient.readData("/test/lock-" + node, stat);
-
-        int version = stat.getVersion();
-
-        log.error("节点数据: {}, 版本: {}", o, version);
-        return o.toString();
+    @RequestMapping("/create-temp-node")
+    public String createTemp(String node) {
+        try {
+            return curatorFramework.create()
+                    .withMode(CreateMode.EPHEMERAL)
+                    .forPath("/" + node, "这是一个临时节点".getBytes());
+        } catch (Exception e) {
+            log.error("zk 创建节点异常: {}", e.getMessage());
+            return "fail: " + e.getMessage();
+        }
     }
 
-    @RequestMapping("/write-data-version")
-    public String helloZk4(String node) {
-        ZkClient zkClient = ZkSessionHolder.getZooKeeper();
+    @RequestMapping("/create-temp-node-p")
+    public String createP(String node) {
+        try {
+            return curatorFramework.create()
+                    .creatingParentContainersIfNeeded()
+                    .forPath("/parent/" + node, "递归创建节点".getBytes());
+        } catch (Exception e) {
+            log.error("zk 创建节点异常: {}", e.getMessage());
+            return "fail: " + e.getMessage();
+        }
+    }
 
-
-        Object readData = zkClient.readData("/test/lock-" + node);
-        zkClient.writeData("/test/lock-" + node, System.currentTimeMillis(), 7);
+    @RequestMapping("/delete")
+    public String delete(String node) {
+        try {
+            curatorFramework.delete()
+                    .forPath("/" + node);
+        } catch (Exception e) {
+            log.error("zk 删除节点异常: {}", e.getMessage());
+            return "fail: " + e.getMessage();
+        }
         return "success";
     }
 
-    @RequestMapping("/write-data")
-    public String helloZk5(String node) {
-        ZkClient zkClient = ZkSessionHolder.getZooKeeper();
-        Object readData = zkClient.readData("/test/lock-" + node);
-        zkClient.writeData("/test/lock-" + node, System.currentTimeMillis());
+
+    @RequestMapping("/get-data")
+    public String getData(String node) {
+        try {
+            byte[] bytes = curatorFramework.getData()
+                    .forPath("/" + node);
+            return new String(bytes);
+        } catch (Exception e) {
+            log.error("zk 获取: {}", e.getMessage());
+            return "fail: " + e.getMessage();
+        }
+    }
+
+    @RequestMapping("/get-data-version")
+    public String getData2(String node) {
+        try {
+            Stat stat = new Stat();
+            byte[] bytes = curatorFramework.getData()
+                    .storingStatIn(stat)
+                    .forPath("/" + node);
+            return node + "节点的版本:" +stat.getVersion();
+        } catch (Exception e) {
+            log.error("zk 获取: {}", e.getMessage());
+            return "fail: " + e.getMessage();
+        }
+    }
+
+    @RequestMapping("/change-data")
+    public String changeData(String node, String data) {
+        try {
+            Stat stat = new Stat();
+            curatorFramework.getData()
+                    .storingStatIn(stat)
+                    .forPath("/" + node);
+            curatorFramework.setData()
+                    .withVersion(stat.getVersion())
+                    .forPath("/" + node, ("节点数据变化为:" + data).getBytes());
+            return "success";
+        } catch (Exception e) {
+            log.error("zk 获取: {}", e.getMessage());
+            return "fail: " + e.getMessage();
+        }
+    }
+
+
+
+    @RequestMapping("/check-exist")
+    public String helloZk4(String node) {
+        try {
+            Stat stat = curatorFramework.checkExists()
+                    .forPath("/" + node);
+            if(stat == null){
+                return "不存在";
+            }
+            return "存在:" + stat.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "fall";
+    }
+
+    @RequestMapping("/write-version")
+    public String helloZk5(String node, Integer version) {
+        try {
+            Stat stat = curatorFramework.setData()
+                    .withVersion(version)
+                    .forPath("/" + node);
+        }catch (KeeperException.BadVersionException e){
+            log.error("设置节点数据版本异常: {}", e.getMessage());
+            return "fall:" + e.getMessage();
+        } catch (Exception e) {
+            log.error("zk异常: {}", e.getMessage());
+            return "fall:" + e.getMessage();
+        }
         return "success";
     }
 
 
     @RequestMapping("/add-observer")
     public String helloZk6(String node) {
-        ZkClient zkClient = ZkSessionHolder.getZooKeeper();
-        Object readData = zkClient.readData("/test/lock-" + node);
-        zkClient.subscribeDataChanges("/test/lock-" + node, new IZkDataListener() {
+        final NodeCache cache = new NodeCache(curatorFramework, "/" + node, false);
+        try {
+            cache.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        cache.getListenable().addListener(new NodeCacheListener() {
             @Override
-            public void handleDataChange(String dataPath, Object data) throws Exception {
-                log.info("dataPath: {}; 数据变为: {}", dataPath, data);
-            }
-
-            @Override
-            public void handleDataDeleted(String dataPath) throws Exception {
-
+            public void nodeChanged() throws Exception {
+                log.error("/" + node + "节点数据有变化" );
             }
         });
-
         return "success";
     }
 
---
+++ webuy-learning/webuy-learning-spring/src/main/java/com/weweibuy/webuy/learning/spring/zk/config/CuratorZkConfig.java
@@ -0,0 +1,63 @@
+package com.weweibuy.webuy.learning.spring.zk.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.*;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.util.List;
+
+/**
+ * @author durenhao
+ * @date 2019/4/22 22:42
+ **/
+@Slf4j
+@Configuration
+public class CuratorZkConfig {
+
+    @Bean
+    @Primary
+    public CuratorFramework curatorFramework(){
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework curatorZkClient = CuratorFrameworkFactory.builder()
+                .connectString("106.12.95.245:2181")
+                .sessionTimeoutMs(60 * 1000)
+                .retryPolicy(retryPolicy)
+                .namespace("learn-spring")
+                .build();
+        curatorZkClient.start();
+
+        //  添加节点监听器
+        final NodeCache cache = new NodeCache(curatorZkClient, "/1", false);
+
+        PathChildrenCache childrenCache = new PathChildrenCache(curatorZkClient, "/", false);
+
+        try {
+            cache.start();
+            childrenCache.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        cache.getListenable().addListener(() -> log.error("1节点数据有变化!"));
+        childrenCache.getListenable().addListener(new PathChildrenCacheListener() {
+            @Override
+            public void childEvent(CuratorFramework curatorFramework, PathChildrenCacheEvent pathChildrenCacheEvent) throws Exception {
+                String path = pathChildrenCacheEvent.getData()
+                        .getPath();
+                List<ChildData> initialData = pathChildrenCacheEvent.getInitialData();
+                byte[] data = pathChildrenCacheEvent.getData()
+                        .getData();
+                log.error("子节点: {} 数据有变化, 数据: {}", path, new String(data));
+            }
+        });
+        return curatorZkClient;
+    }
+
+
+}
