ba429b1b10accad537cac79f18e0ce0415dc5aba
杜仁浩 <ak514250@126.com>
2019-08-31 17:14:29 +0800

    测试 rocketmq

--- webuy-learning/webuy-learning-mq/webuy-learning-mq-consumer/src/test/java/com/weweibuy/webuy/learing/mq/consumer/MqConsumerApplicationTest.java
+++ webuy-learning/webuy-learning-mq/webuy-learning-mq-consumer/src/test/java/com/weweibuy/webuy/learing/mq/consumer/MqConsumerApplicationTest.java
@@ -2,14 +2,13 @@ package com.weweibuy.webuy.learing.mq.consumer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
-import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
-import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.consumer.listener.*;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class MqConsumerApplicationTest {
@@ -18,34 +17,121 @@ public class MqConsumerApplicationTest {
 
     private static final String TOPIC = "test_producer_topic";
 
-    private String producerGroup = "test_producer_group";
+    private String producerGroup1 = "test_producer_group1";
 
-    private DefaultMQPushConsumer consumer;
+    private String producerGroup2 = "test_producer_group2";
+
+
+    private DefaultMQPushConsumer consumerG1;
+
+    private DefaultMQPushConsumer consumerG2;
+
 
     @Before
     public void bf() throws Exception {
-        consumer = new DefaultMQPushConsumer(producerGroup);
-        consumer.setNamesrvAddr(NAME_SERVER);
-        consumer.subscribe(TOPIC, "*");
+        consumerG1 = new DefaultMQPushConsumer(producerGroup1);
+        consumerG1.setNamesrvAddr(NAME_SERVER);
+        consumerG1.subscribe(TOPIC, "abc");
+
+        consumerG2 = new DefaultMQPushConsumer(producerGroup2);
+        consumerG2.setNamesrvAddr(NAME_SERVER);
+        consumerG2.subscribe(TOPIC, "test");
+
     }
 
     @Test
     public void test01() throws Exception {
-        consumer.registerMessageListener(new MessageListenerConcurrently() {
+        consumerG1.registerMessageListener(new MessageListenerConcurrently() {
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
                 msgs.stream().forEach(msg -> {
-                    log.info("收到消息: {}", new String(msg.getBody()));
+                    log.info("消费组1 Listener1 收到消息: {}", new String(msg.getBody()));
                 });
-                return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
         });
-        consumer.start();
-        while (true){
+
+        consumerG1.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                msgs.stream().forEach(msg -> {
+                    log.info("消费组1 Listener2 收到消息: {}", new String(msg.getBody()));
+                });
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        consumerG1.start();
+
+        consumerG2.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                msgs.stream().forEach(msg -> {
+                    int ackIndex = context.getAckIndex();
+                    log.info("ackIndex : {}", ackIndex);
+                    log.info("消费组2 收到消息: {}, 数量: {}; messageId: {}, 队列id: {}", new String(msg.getBody()), msgs.size(),
+                            msg.getMsgId(), msg.getQueueId());
+                });
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumerG2.start();
+        while (true) {
             Thread.sleep(500);
         }
-
     }
 
+
+    @Test
+    public void test02() throws Exception {
+        consumerG1.registerMessageListener(new MessageListenerOrderly() {
+            @Override
+            public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext context) {
+                msgs.stream().forEach(msg -> {
+                    boolean autoCommit = context.isAutoCommit();
+                    log.info("消费组1 收到消息: {}, messageId: {}, 队列id: {}", new String(msg.getBody()),
+                            msg.getMsgId(), msg.getQueueId());
+                });
+                return ConsumeOrderlyStatus.SUCCESS;
+            }
+        });
+        consumerG1.start();
+        while (true) {
+            Thread.sleep(500);
+        }
+    }
+
+
+    @Test
+    public void test03() throws Exception {
+        consumerG2.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                MessageExt messageExt = msgs.get(0);
+                Map<String, String> properties = messageExt.getProperties();
+                try {
+                    msgs.stream().forEach(msg -> {
+                        log.info("消费组1 Listener1 收到消息: {}; id: {}", new String(msg.getBody()), msg.getMsgId());
+                        throw new RuntimeException();
+                    });
+                } catch (Exception e) {
+                    String origin_message_id = properties.get("ORIGIN_MESSAGE_ID");
+                    int reconsumeTimes = messageExt.getReconsumeTimes();
+                    log.info("消息属性: {}", properties);
+                    log.info("原始msgId: {}", origin_message_id);
+                    log.info("重试次数: {}", reconsumeTimes);
+                    if(reconsumeTimes >= 3){
+                        return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                    }
+                    return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumerG2.start();
+        while (true) {
+            Thread.sleep(500);
+        }
+    }
 
 }
--- webuy-learning/webuy-learning-mq/webuy-learning-mq-producer/src/test/java/com/weweibuy/webuy/learing/mq/producer/MqProducerApplicationTest.java
+++ webuy-learning/webuy-learning-mq/webuy-learning-mq-producer/src/test/java/com/weweibuy/webuy/learing/mq/producer/MqProducerApplicationTest.java
@@ -2,11 +2,16 @@ package com.weweibuy.webuy.learing.mq.producer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @Slf4j
 public class MqProducerApplicationTest {
@@ -28,12 +33,52 @@ public class MqProducerApplicationTest {
 
     @Test
     public void test01() throws Exception {
-        Message msg = new Message(TOPIC, "test",
-                ("Hello RocketMQ 3").getBytes(RemotingHelper.DEFAULT_CHARSET));
-        SendResult send = producer.send(msg);
-        String msgId = send.getMsgId();
-        log.info("messageId: {}", msgId);
-        log.info("{}", send);
+        Stream.iterate(0, n -> n + 1)
+                .limit(10)
+                .forEach(i -> {
+                    Message msg = null;
+                    try {
+                        msg = new Message(TOPIC, "test",
+                                ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                        SendResult send = producer.send(msg);
+                        String msgId = send.getMsgId();
+                        int queueId = send.getMessageQueue().getQueueId();
+                        log.info("发送消息 messageId: {} 成功; 队列: {}", msgId, queueId);
+                        log.info("{}", send);
+                    } catch (Exception e) {
+                    }
+
+                });
+
+    }
+
+
+    /**
+     * 向指定队列发送
+     * @throws Exception
+     */
+    @Test
+    public void test02() throws Exception {
+        Stream.iterate(0, n -> n + 1)
+                .limit(1000)
+                .forEach(i -> {
+                    Message msg = null;
+                    try {
+                        msg = new Message(TOPIC, "test",
+                                ("Hello RocketMQ  queue" + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                        SendResult send = producer.send(msg, new MessageQueueSelector() {
+                            @Override
+                            public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                                return mqs.get((Integer) arg);
+                            }
+                        }, 0);// 0 --> 队列下表
+                        String msgId = send.getMsgId();
+                        int queueId = send.getMessageQueue().getQueueId();
+                        log.info("发送消息 messageId: {} 成功; 队列: {}", msgId, queueId);
+                        log.info("{}", send);
+                    } catch (Exception e) {
+                    }
+                });
 
     }
 
