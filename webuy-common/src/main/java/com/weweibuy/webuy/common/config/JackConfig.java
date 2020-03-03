d7ccb42c382c40207e8cae7454443eebcc4d4eed
杜仁浩 <ak514250@126.com>
2020-03-03 23:30:59 +0800

    更改 jackson 工具

---
+++ webuy-common/src/main/java/com/weweibuy/webuy/common/config/JackConfig.java
@@ -0,0 +1,20 @@
+package com.weweibuy.webuy.common.config;
+
+import com.weweibuy.webuy.common.utils.JackJsonUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author durenhao
+ * @date 2020/3/1 10:57
+ **/
+@Configuration
+public class JackConfig {
+
+
+    @Bean
+    public JackJsonUtils jackJsonUtils(){
+        return new JackJsonUtils();
+    }
+
+}
--- webuy-common/src/main/java/com/weweibuy/webuy/common/utils/JackJsonUtils.java
+++ webuy-common/src/main/java/com/weweibuy/webuy/common/utils/JackJsonUtils.java
@@ -3,9 +3,12 @@ package com.weweibuy.webuy.common.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 
 /**
@@ -15,19 +18,91 @@ import java.io.IOException;
 @Component
 public class JackJsonUtils {
 
-    private static ObjectMapper objectMapper;
+    private static ObjectMapper CAMEL_CASE_MAPPER;
+
+    private static ObjectMapper SNAKE_CASE_MAPPER;
+
+    private static ObjectMapper MVC_OBJECT_MAPPER;
 
     @Autowired
-    private ObjectMapper mapper;
+    private ObjectMapper objectMapper;
 
     @Autowired
+    private Jackson2ObjectMapperBuilder objectMapperBuilder;
+
+    @PostConstruct
     public void init() {
-        objectMapper = mapper;
+        MVC_OBJECT_MAPPER = objectMapper;
+        PropertyNamingStrategy propertyNamingStrategy = objectMapper.getPropertyNamingStrategy();
+
+        if (propertyNamingStrategy == null) {
+            CAMEL_CASE_MAPPER = objectMapper;
+            SNAKE_CASE_MAPPER = objectMapperBuilder.createXmlMapper(false)
+                    .propertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                    .build();
+        } else if (PropertyNamingStrategy.SNAKE_CASE.getClass().getSimpleName()
+                .equals(propertyNamingStrategy.getClass().getSimpleName())) {
+            SNAKE_CASE_MAPPER = objectMapper;
+            CAMEL_CASE_MAPPER = objectMapperBuilder.createXmlMapper(false)
+                    .propertyNamingStrategy(PropertyNamingStrategy.UPPER_CAMEL_CASE)
+                    .build();
+        } else {
+            throw new IllegalStateException("Jackson objectMapper NamingStrategy 为: " + propertyNamingStrategy.getClass().getSimpleName() + "目前不支持");
+        }
+    }
+
+    public static <T> T snakeCaseReadValue(String json, Class<? extends T> clazz) {
+        try {
+            return MVC_OBJECT_MAPPER.readValue(json, clazz);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T snakeCaseReadValue(String json, JavaType javaType) {
+        try {
+            return MVC_OBJECT_MAPPER.readValue(json, javaType);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T snakeCaseReadValue(byte[] json, Class<? extends T> clazz) {
+        try {
+            return MVC_OBJECT_MAPPER.readValue(json, clazz);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T snakeCaseReadValue(byte[] json, JavaType javaType) {
+        try {
+            return MVC_OBJECT_MAPPER.readValue(json, javaType);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static String snakeCaseWrite(Object object) {
+        try {
+            return MVC_OBJECT_MAPPER.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static byte[] snakeCaseWriteAsByte(Object object) {
+        try {
+            return MVC_OBJECT_MAPPER.writeValueAsBytes(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static <T> T readValue(String json, Class<? extends T> clazz) {
         try {
-            return objectMapper.readValue(json, clazz);
+            return MVC_OBJECT_MAPPER.readValue(json, clazz);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -35,7 +110,7 @@ public class JackJsonUtils {
 
     public static <T> T readValue(String json, JavaType javaType) {
         try {
-            return objectMapper.readValue(json, javaType);
+            return MVC_OBJECT_MAPPER.readValue(json, javaType);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -43,7 +118,7 @@ public class JackJsonUtils {
 
     public static <T> T readValue(byte[] json, Class<? extends T> clazz) {
         try {
-            return objectMapper.readValue(json, clazz);
+            return MVC_OBJECT_MAPPER.readValue(json, clazz);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -51,7 +126,7 @@ public class JackJsonUtils {
 
     public static <T> T readValue(byte[] json, JavaType javaType) {
         try {
-            return objectMapper.readValue(json, javaType);
+            return MVC_OBJECT_MAPPER.readValue(json, javaType);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -60,7 +135,7 @@ public class JackJsonUtils {
 
     public static String write(Object object) {
         try {
-            return objectMapper.writeValueAsString(object);
+            return MVC_OBJECT_MAPPER.writeValueAsString(object);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
@@ -68,10 +143,72 @@ public class JackJsonUtils {
 
     public static byte[] writeAsByte(Object object) {
         try {
-            return objectMapper.writeValueAsBytes(object);
+            return MVC_OBJECT_MAPPER.writeValueAsBytes(object);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
 
+
+    public static <T> T camelCaseReadValue(String json, Class<? extends T> clazz) {
+        try {
+            return MVC_OBJECT_MAPPER.readValue(json, clazz);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T camelCaseReadValue(String json, JavaType javaType) {
+        try {
+            return MVC_OBJECT_MAPPER.readValue(json, javaType);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T camelCaseReadValue(byte[] json, Class<? extends T> clazz) {
+        try {
+            return MVC_OBJECT_MAPPER.readValue(json, clazz);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T camelCaseReadValue(byte[] json, JavaType javaType) {
+        try {
+            return MVC_OBJECT_MAPPER.readValue(json, javaType);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static String camelCaseWrite(Object object) {
+        try {
+            return MVC_OBJECT_MAPPER.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static byte[] camelCaseWriteAsByte(Object object) {
+        try {
+            return MVC_OBJECT_MAPPER.writeValueAsBytes(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static ObjectMapper getCamelCaseMapper() {
+        return CAMEL_CASE_MAPPER;
+    }
+
+    public static ObjectMapper getSnakeCaseMapper() {
+        return SNAKE_CASE_MAPPER;
+    }
+
+    public static ObjectMapper getMvcObjectMapper() {
+        return MVC_OBJECT_MAPPER;
+    }
 }
