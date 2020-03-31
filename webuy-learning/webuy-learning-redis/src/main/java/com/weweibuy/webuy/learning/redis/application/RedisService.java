42c1533aaaf766937e4dd4ae75310227b23fea74
suxing.zhang <suxing.zhang@pousheng.com>
2020-03-31 23:20:28 +0800

    redis 分布式锁实现

--- webuy-learning/webuy-learning-redis/src/main/java/com/weweibuy/webuy/learning/redis/application/RedisService.java
+++ webuy-learning/webuy-learning-redis/src/main/java/com/weweibuy/webuy/learning/redis/application/RedisService.java
@@ -1,5 +1,7 @@
 package com.weweibuy.webuy.learning.redis.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,4 +10,12 @@ import org.springframework.stereotype.Service;
  **/
 @Service
 public class RedisService {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    //todo 分布式锁的实现 spring-boot-strart 版本不是2.0以上  可使用lua script 替代
+    public void mockDistributeLock(String orderNo, String bizFlowNo) {
+        stringRedisTemplate.opsForValue().setIfAbsent("", "");
+    }
 }
