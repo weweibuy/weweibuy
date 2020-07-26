0003ca9a0a09d2ef854d7cff38a16b83b52bde0b
杜仁浩 <ak514250@126.com>
2020-07-26 15:44:52 +0800

    文件上传下载测试

--- webuy-learning/webuy-learning-feign-provider/src/main/java/com/weweibuy/learning/feign/provider/controller/HelloWorldController.java
+++ webuy-learning/webuy-learning-feign-provider/src/main/java/com/weweibuy/learning/feign/provider/controller/HelloWorldController.java
@@ -3,7 +3,17 @@ package com.weweibuy.learning.feign.provider.controller;
 import com.weweibuy.framework.common.core.exception.Exceptions;
 import com.weweibuy.framework.common.core.model.dto.CommonDataJsonResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author durenhao
@@ -21,16 +31,32 @@ public class HelloWorldController {
 
     @PostMapping("/hello")
     public CommonDataJsonResponse<String> helloPost(@RequestBody CommonDataJsonResponse<String> commonJsonResponse,
-                                                @RequestHeader(value = "token") String token,
-                                                String sign) {
+                                                    @RequestHeader(value = "token") String token,
+                                                    String sign) {
         log.info("date : {}", commonJsonResponse.getData());
         log.info("token : {}", token);
         log.info("sign : {}", sign);
-        if(true){
+        if (true) {
             throw Exceptions.unknown();
         }
         return CommonDataJsonResponse.success("hello i am provider post method");
     }
 
+    @PostMapping("/hello-file")
+    public ResponseEntity<StreamingResponseBody> helloFile(MultipartFile file, String name, HttpServletResponse response) throws IOException {
+        InputStream inputStream = file.getInputStream();
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + name)
+                .body(outputStream ->
+                {
+                    byte[] buf = new byte[1024];
+                    int len = 0;
+                    while ((len = inputStream.read(buf)) != -1) {
+                        outputStream.write(buf);
+                    }
+                });
+
+    }
 
 }
