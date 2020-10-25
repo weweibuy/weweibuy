7f0fe5a9155e1e2170bc89c134efac0d307ef75a
杜仁浩 <ak514250@126.com>
2020-10-25 22:55:10 +0800

    drool-spring

---
+++ webuy-learning/webuy-learning-drools/src/main/java/com/weweibuy/webuy/learning/drools/LearningDroolsApplication.java
@@ -0,0 +1,17 @@
+package com.weweibuy.webuy.learning.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author durenhao
+ * @date 2020/10/25 22:47
+ **/
+@SpringBootApplication
+public class LearningDroolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningDroolsApplication.class, args);
+    }
+
+}
